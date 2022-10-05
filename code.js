8e8caf0f0dc35c5daba8a0ca0b9fc8972d1bb48c
Athæryn Anderson <athaeryn@users.noreply.github.com>
2022-10-05 12:08:38 -0500

    Update code.js
    Change implementation slightly so that floors are removed from both queues when the elevator stops on them.
    Previously, checking for a floor to go to would remove the floor from that queue.

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 {
     init: function(elevators, floors) {
         let elevator = elevators[0];
-        
+
         // Floors where passengers are waiting.
         let waitingFloors = [];
-        
+
         // Floors where passengers want to go.
         let deliveryFloors = [];
-        
+
         // When a button is pressed on a floor, add it to waitingFloors.
         floors.forEach(function(floor) {
             let floorNumber = floor.floorNum();
@@ -19,28 +19,26 @@
             floor.on("up_button_pressed", onButtonPressed.bind(null, "up"));
             floor.on("down_button_pressed", onButtonPressed.bind(null, "down"));
         })
-        
+
         // When a button in the elevator is pressed, add to deliveryFloors.
         elevator.on("floor_button_pressed", function(floorNumber) {
             if (!deliveryFloors.includes(floorNumber)) {
                 deliveryFloors.push(floorNumber);
             }
         })
-        
-        function getWaitingFloor() {
-            return waitingFloors.shift();
-        }
-        
-        function getDeliveryFloor() {
-            return deliveryFloors.shift();
-        }
 
         // When the elevator is idle, first try and drop off passengers, then try and pick up more.
         elevator.on("idle", function() {
-            let floor = getDeliveryFloor() || getWaitingFloor();
+            let floor = deliveryFloors[0] || waitingFloors[0];
             if (floor != null) {
                 elevator.goToFloor(floor);
             }
+        });
+        
+        // Remove floors from the queues when the elevator stops at them.
+        elevator.on("stopped_at_floor", function(floorNumber) {
+            waitingFloors = waitingFloors.filter(x => x !== floorNumber);
+            deliveryFloors = deliveryFloors.filter(x => x !== floorNumber);
         });
     },
     update: function(dt, elevators, floors) {}
