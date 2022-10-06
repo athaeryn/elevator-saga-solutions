fd4db5e9b7f0e654a65084b91c4030234fd2e5a4
Athaeryn <ryn@athaeryn.com>
2022-10-06 15:23:06 -0500

    Solve Challenge #4
    The queue with floors where passengers are waiting is shared, and each
    elevator has its own queue of passenger destination floors.

--- code.js
+++ code.js
@@ -1,45 +1,45 @@
-{
-    init: function(elevators, floors) {
-        let elevator = elevators[0];
+({
+  init: function (elevators, floors) {
+    // Floors where passengers are waiting.
+    let waitingFloors = [];
 
-        // Floors where passengers are waiting.
-        let waitingFloors = [];
+    // When a button is pressed on a floor, add it to waitingFloors.
+    floors.forEach((floor) => {
+      let floorNumber = floor.floorNum();
+      let onButtonPressed = (direction) => {
+        if (!waitingFloors.includes(floorNumber)) {
+          waitingFloors.push(floorNumber);
+        }
+      };
+      floor.on("up_button_pressed", onButtonPressed.bind(null, "up"));
+      floor.on("down_button_pressed", onButtonPressed.bind(null, "down"));
+    });
 
-        // Floors where passengers want to go.
-        let deliveryFloors = [];
+    elevators.forEach((elevator) => {
+      // Floors where passengers in this elevator want to go.
+      let deliveryFloors = [];
 
-        // When a button is pressed on a floor, add it to waitingFloors.
-        floors.forEach(function(floor) {
-            let floorNumber = floor.floorNum();
-            function onButtonPressed(direction) {
-                if (!waitingFloors.includes(floorNumber)) {
-                    waitingFloors.push(floorNumber);
-                }
-            }
-            floor.on("up_button_pressed", onButtonPressed.bind(null, "up"));
-            floor.on("down_button_pressed", onButtonPressed.bind(null, "down"));
-        })
+      // When a button in the elevator is pressed, add to deliveryFloors.
+      elevator.on("floor_button_pressed", function (floorNumber) {
+        if (!deliveryFloors.includes(floorNumber)) {
+          deliveryFloors.push(floorNumber);
+        }
+      });
 
-        // When a button in the elevator is pressed, add to deliveryFloors.
-        elevator.on("floor_button_pressed", function(floorNumber) {
-            if (!deliveryFloors.includes(floorNumber)) {
-                deliveryFloors.push(floorNumber);
-            }
-        })
+      // When the elevator is idle, first try and drop off passengers, then try and pick up more.
+      elevator.on("idle", function () {
+        let floor = deliveryFloors[0] || waitingFloors[0];
+        if (floor != null) {
+          elevator.goToFloor(floor);
+        }
+      });
 
-        // When the elevator is idle, first try and drop off passengers, then try and pick up more.
-        elevator.on("idle", function() {
-            let floor = deliveryFloors[0] || waitingFloors[0];
-            if (floor != null) {
-                elevator.goToFloor(floor);
-            }
-        });
-        
-        // Remove floors from the queues when the elevator stops at them.
-        elevator.on("stopped_at_floor", function(floorNumber) {
-            waitingFloors = waitingFloors.filter(x => x !== floorNumber);
-            deliveryFloors = deliveryFloors.filter(x => x !== floorNumber);
-        });
-    },
-    update: function(dt, elevators, floors) {}
-}
+      // Remove floors from the queues when the elevator stops at them.
+      elevator.on("stopped_at_floor", function (floorNumber) {
+        waitingFloors = waitingFloors.filter((x) => x !== floorNumber);
+        deliveryFloors = deliveryFloors.filter((x) => x !== floorNumber);
+      });
+    });
+  },
+  update: function (dt, elevators, floors) {},
+});
