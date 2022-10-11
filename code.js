dee860ba571eaa8069177fa315e83bb7adf629fc
Athaeryn <ryn@athaeryn.com>
2022-10-11 13:08:30 -0500

    Attempt #5
    This wasn't quite enough to pass Challenge #5. I saw results in the high
    90s but never quite reached 100.
    
    The biggest difference is that I'm now tracking whether the up or down
    button was pressed, and setting the up/down indicators on each elevator
    to affect which passengers board when stopping on a floor.
    
    Occasionally an elevator would stop on a floor and not start moving
    again. I'm assuming this happens when there's nothing tracked in the
    waitingFloors queue at the time the "idle" event is processed. There's
    nothing to tell the elevator to try again.
    
    I think the best move forward is to mark the elevator as idle, then in
    the update function check if we have any idle elevators and assign
    destination queues there.

--- code.js
+++ code.js
@@ -1,45 +1,106 @@
 ({
   init: function (elevators, floors) {
+    const UP = "up";
+    const DOWN = "down";
+
     // Floors where passengers are waiting.
-    let waitingFloors = [];
+    let waitingFloors = {
+      all: [],
+      add: function (floorNumber, direction) {
+        if (
+          !this.all.find(
+            (x) => x.floor === floorNumber && x.direction === direction
+          )
+        ) {
+          this.all.push({
+            floor: floorNumber,
+            direction,
+          });
+        }
+      },
+      remove: function (floorNumber, direction) {
+        this.all = this.all.filter(
+          (x) => !(x.floor === floorNumber && x.direction === direction)
+        );
+      },
+      getByDirection: function (direction) {
+        return this.all.filter((x) => x.direction === direction);
+      },
+    };
 
     // When a button is pressed on a floor, add it to waitingFloors.
     floors.forEach((floor) => {
       let floorNumber = floor.floorNum();
       let onButtonPressed = (direction) => {
-        if (!waitingFloors.includes(floorNumber)) {
-          waitingFloors.push(floorNumber);
-        }
+        waitingFloors.add(floorNumber, direction);
       };
-      floor.on("up_button_pressed", onButtonPressed.bind(null, "up"));
-      floor.on("down_button_pressed", onButtonPressed.bind(null, "down"));
+      floor.on("up_button_pressed", onButtonPressed.bind(null, UP));
+      floor.on("down_button_pressed", onButtonPressed.bind(null, DOWN));
     });
 
     elevators.forEach((elevator) => {
-      // Floors where passengers in this elevator want to go.
-      let deliveryFloors = [];
+      let travelDirection = UP;
 
-      // When a button in the elevator is pressed, add to deliveryFloors.
+      setElevatorDirectionIndicators(elevator, travelDirection);
+
+      // When a button in the elevator is pressed, ensure it is in destinationQueue.
       elevator.on("floor_button_pressed", function (floorNumber) {
-        if (!deliveryFloors.includes(floorNumber)) {
-          deliveryFloors.push(floorNumber);
+        // assumes button pressed will be in same direction as travel
+        if (!elevator.destinationQueue.includes(floorNumber)) {
+          elevator.destinationQueue.push(floorNumber);
+          elevator.destinationQueue.sort((a, b) => {
+            if (travelDirection === UP) {
+              return a - b;
+            } else {
+              return b - a;
+            }
+          });
+          elevator.checkDestinationQueue();
         }
       });
 
-      // When the elevator is idle, first try and drop off passengers, then try and pick up more.
       elevator.on("idle", function () {
-        let floor = deliveryFloors[0] || waitingFloors[0];
-        if (floor != null) {
-          elevator.goToFloor(floor);
-        }
-      });
+        // Are we closer to the top or bottom
+        let currentFloor = elevator.currentFloor();
+        // If the distance to the top is less than the distance to the bottom
+        let closerToTop = Math.abs(currentFloor - floors.length) < currentFloor;
 
-      // Remove floors from the queues when the elevator stops at them.
-      elevator.on("stopped_at_floor", function (floorNumber) {
-        waitingFloors = waitingFloors.filter((x) => x !== floorNumber);
-        deliveryFloors = deliveryFloors.filter((x) => x !== floorNumber);
+        travelDirection = closerToTop ? DOWN : UP;
+
+        let destinations = waitingFloors.getByDirection(travelDirection);
+
+        // If we have nowhere to go in this direction, try the other direction.
+        if (destinations.length === 0) {
+          let otherDirection = travelDirection === UP ? DOWN : UP;
+          let x = waitingFloors.getByDirection(otherDirection);
+          if (x.length > 0) {
+            travelDirection = otherDirection;
+            destinations = x;
+          }
+        }
+
+        setElevatorDirectionIndicators(elevator, travelDirection);
+
+        destinations.sort((a, b) => {
+          if (travelDirection === UP) {
+            return a.floor - b.floor;
+          } else {
+            return b.floor - a.floor;
+          }
+        });
+
+        // Remove destinations from waiting floors proactively
+        destinations.forEach((a) => waitingFloors.remove(a.floor, a.direction));
+
+        elevator.destinationQueue = destinations.map((y) => y.floor);
+        elevator.checkDestinationQueue();
       });
     });
+
+    function setElevatorDirectionIndicators(elevator, direction) {
+      elevator.goingUpIndicator(direction === UP);
+      elevator.goingDownIndicator(direction === DOWN);
+    }
   },
   update: function (dt, elevators, floors) {},
 });
